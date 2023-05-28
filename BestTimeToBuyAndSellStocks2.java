9d2902184437be197b952d06d61a654ba536fb5f
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-05-28 22:25:59 +0530

    day 12

---
+++ BestTimeToBuyAndSellStocks2.java
@@ -0,0 +1,16 @@
+public class BestTimeToBuyAndSellStocks2 {
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int profit = 0;
+            int minPrice = Integer.MAX_VALUE;
+            for (int i = 0; i < prices.length; i++) {
+                minPrice = Math.min(minPrice, prices[i]);
+                if (prices[i] - minPrice > 0) {
+                    profit += prices[i] - minPrice;
+                    minPrice = prices[i];
+                }
+            }
+            return profit;
+        }
+    }
+}
