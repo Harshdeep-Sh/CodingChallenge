81d702fa75e45d7d77145a301c5df680212f08b7
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-06-04 21:55:21 +0530

    day 17

---
+++ ConcatenateArray.java
@@ -0,0 +1,10 @@
+public class ConcatenateArray {
+    public int[] getConcatenation(int[] nums) {
+        int[] arr = new int[2*nums.length];
+
+        for(int i = 0;i<2*nums.length;i++){
+            arr[i] = nums[i%nums.length];
+        }
+        return arr;
+    }
+}
