631d45ea90e8725cb9b25dae5393a8e6c3c121b1
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-06-06 12:49:31 +0530

    day 19

---
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> hs = new HashSet<>();
+        int c = 0;
+        int prev_size = 0; 
+        for(int i = 0;i<s.length();i++){
+            hs.add((s.charAt(i)));
+            if(hs.size() == prev_size){
+                i=i-hs.size();
+                hs.clear();
+            }
+            c = Math.max(c,hs.size());
+            prev_size = hs.size();        
+        }
+        return c;
+    }
+}
