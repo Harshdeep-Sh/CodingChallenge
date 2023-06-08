6e6cb7299f96a24430a8235cfd4c2feac9c5c496
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-06-08 22:29:22 +0530

    day 21

---
+++ CountNegativeNumbersInGrid.java
@@ -0,0 +1,14 @@
+public class CountNegativeNumbersInGrid {
+    public int countNegatives(int[][] grid) {
+        int c = 0;
+        // basic solution
+        for(int[] i:grid){
+            for(int j:i){
+                if(j<0){
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}
