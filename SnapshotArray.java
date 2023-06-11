a0f9115945a1124fdbe5e6f8d67323732e3d3c53
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-06-11 21:21:36 +0530

    day 23

--- SnapshotArray.java
+++ SnapshotArray.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+// day 23 -  Leetcode problem 1146: Snapshot Array
 public class SnapshotArray {
     List<Map<Integer,Integer>> lst;
     public SnapshotArray(int length) {
