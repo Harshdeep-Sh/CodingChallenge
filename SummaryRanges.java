99ed7ea2ab2197d6349e69f24476ad51ad7bd8f8
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-06-12 22:44:06 +0530

    day 24

---
+++ SummaryRanges.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SummaryRanges {
+        public List<String> summaryRanges(int[] nums) {
+        if(nums==null || nums.length==0){
+            return new ArrayList<>();
+        }
+
+        List<String> arr = new ArrayList<>();
+        int init = nums[0];
+        for(int i = 1;i<nums.length;i++){
+            if(nums[i]!=nums[i-1]+1){
+                if(init==nums[i-1]){
+                    arr.add(Integer.toString(init));
+                }
+                else{
+                    arr.add(init+"->"+nums[i-1]);
+                }
+                init = nums[i];
+            }
+        }
+        if(init==nums[nums.length-1]){
+            arr.add(Integer.toString(init));
+        }
+        else{
+            arr.add(init + "->" + nums[nums.length-1]);
+        }
+        return arr;
+    }
+}
