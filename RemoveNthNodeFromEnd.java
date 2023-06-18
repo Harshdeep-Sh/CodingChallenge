3dad6c652d81698cb1e4b26fa3d13e1bd82fd8b7
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-06-18 22:16:34 +0530

    day 30

--- RemoveNthNodeFromEnd.java
+++ RemoveNthNodeFromEnd.java
@@ -5,7 +5,7 @@ class ListNode {
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
   }
- 
+// day 30 - Leetcode Problem 19: Remove Nth Element From the List
 
 public class RemoveNthNodeFromEnd {
     public ListNode removeNthFromEnd(ListNode head, int n) {
