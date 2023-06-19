3669674432feb529e94d9d98a2f7bc9d8c3535e1
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-06-19 22:31:03 +0530

    day 31

---
+++ RotateList.java
@@ -0,0 +1,34 @@
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+public class RotateList{
+    public ListNode rotateRight(ListNode head, int k) {
+        int len = 1;
+        ListNode node = head;
+        if(node==null){
+            return head;
+        }
+        while(node.next!=null){
+            len+=1;
+            node = node.next;
+        }
+        k = k%len;
+
+        if(k==0 || len<=1){
+            return head;
+        } 
+        ListNode iter = head;
+        for(int i = 0;i<len-k-1;i++){
+            iter = iter.next;
+        }
+        node.next = head;
+        head = iter.next;
+        iter.next = null;
+        return head;
+    }
+}
