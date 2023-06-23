e008b55c2df38e9b91bde84a14ab26e43815ed23
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-06-23 22:41:17 +0530

    day 35

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,24 @@
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int area = 0;
+        int left = 0;
+        int right= height.length-1;
+        while(left<right){
+            
+            if(height[left]<height[right]){
+                area = Math.max((right-left)*height[left],area);
+                left++;
+            }
+            else if(height[left]>height[right]){
+                area = Math.max((right-left)*height[right],area);
+                right--;
+            }
+            else if(height[left]==height[right]){
+                area = Math.max((right-left)*height[left],area);
+                left++;
+                right--;
+            }
+        }
+        return area;
+    }
+}
