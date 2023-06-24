71e2776fb48ff44eda02da1cdac1ea0d0f75b630
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-06-24 23:04:18 +0530

    day 35

--- JumpGame1.java
+++ JumpGame1.java
@@ -1,13 +1,11 @@
 public class JumpGame1 {
     public boolean canJump(int[] nums) {
-        int ans = 0;
         int curr=nums[0];
         int index=0;
         while(curr!=0 && index!=nums.length-1){
             curr--;
             index++;
             if(nums[index]>curr){
-                ans++;
                 curr = nums[index];
             }
         }
