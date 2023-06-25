4e9c18ae64e7f4c1623371f34e8de70279bea535
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-06-25 23:41:49 +0530

    day 37

---
+++ MergeNodeBetweenZeros.java
@@ -0,0 +1,19 @@
+public class MergeNodeBetweenZeros {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode ans = new ListNode(0);
+        ListNode temp = ans;
+        int sum = 0;
+        while(head!=null){
+            if(head.val==0){
+                temp.next = new ListNode(sum);
+                temp = temp.next;
+                sum = 0;
+            }
+            else{
+                sum+=head.val;
+            }
+            head = head.next;
+        }
+        return ans.next.next;
+    }
+}
