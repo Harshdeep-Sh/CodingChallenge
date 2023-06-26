1a7f19a6ac95064f223406b664d847d9b17ddeef
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-06-26 23:32:00 +0530

    day 38

---
+++ ReverseWordsInString.java
@@ -0,0 +1,26 @@
+public class ReverseWordsInString{
+    public String reverseWords(String s) {
+        String ans = "";
+        String container = "";
+        for(int i = s.length()-1;i>=0;i--){
+            if(s.charAt(i)==' ' && container ==""){
+                continue;
+            }
+            else if(s.charAt(i)==' '){
+                ans+=" "+container;
+                container = "";
+            }
+            else{
+                container = s.charAt(i)+container;
+            }
+        }
+        ans = ans+" "+container;
+        if(ans.charAt(0)==' '){
+            ans = ans.substring(1);
+        }
+        if(ans.charAt(ans.length()-1)==' '){
+            ans = ans.substring(0,ans.length()-1);
+        }
+        return ans;
+    }
+}
