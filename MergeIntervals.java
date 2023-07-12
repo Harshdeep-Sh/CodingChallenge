aee8aa4e15d42f175de0dfcf872985366e3146f3
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-07-12 23:33:56 +0530

    Day 47

---
+++ MergeIntervals.java
@@ -0,0 +1,28 @@
+// Day 47 - LeetCode Problem 56: Merge Intervals
+import java.util.*;
+public class MergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return new int[0][];
+        }
+
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> ans = new ArrayList<>();
+        int[] curr = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= curr[1]) {
+                curr[1] = Math.max(curr[1], intervals[i][1]);
+            } else {
+                ans.add(curr);
+                curr = intervals[i]; 
+            }
+        }
+
+        ans.add(curr);
+
+        return ans.toArray(new int[ans.size()][]);
+
+    }
+}
