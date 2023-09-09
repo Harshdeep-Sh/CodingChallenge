05c5241693b7f2304ef643282edfccf35231c782
Harshdeep Shrivastava <harshri2015@gmail.com>
2023-09-09 22:12:55 +0530

    Day 69

---
+++ CombinationSumIV.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class CombinationSumIV {
+    public int solve(int target,int[] nums,int[] arr){
+        if(arr[target]!=-1){
+            return arr[target];
+        }
+        if(0==target){
+            return 1;
+        }
+        int sum = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(target-nums[i]>=0){
+                sum+=solve(target-nums[i],nums,arr);
+            }
+        }
+        arr[target] = sum;
+        return sum;
+    }
+    public int combinationSum4(int[] nums, int target) {
+        int[] arr = new int[target+1];
+        Arrays.fill(arr,-1);
+        return solve(target,nums,arr);
+    }
+}
